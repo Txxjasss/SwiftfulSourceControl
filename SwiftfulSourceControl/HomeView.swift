f992f4be471c2010eb5b568c0040f5e304ac47b9
Txxjasss <tejasjanagi2006@gmail.com>
2025-08-13 17:54:00 +0530

    Add HomevVew

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Tejas Janagi on 13/08/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
