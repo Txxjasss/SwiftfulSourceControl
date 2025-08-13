94243f7b04995e880fca56c7cf6b001a1e9ab784
Txxjasss <tejasjanagi2006@gmail.com>
2025-08-13 19:09:19 +0530

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,26 @@
 //  Created by Tejas Janagi on 13/08/25.
 //
 
+
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ */
+
 import SwiftUI
 
 struct ContentView: View {
