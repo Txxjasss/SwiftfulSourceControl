8dc9a9b18af0ed650a9f222cc16c89ede7d6ab92
Txxjasss <tejasjanagi2006@gmail.com>
2025-08-13 23:01:33 +0530

    [Feature] Add Helloooo text

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var title: String = ""
+    @State private var title: String = "Helloooo"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hi")
         Text("Screen 2")
+        
+        Text("Screen 3")
+        
     }
 }
 
